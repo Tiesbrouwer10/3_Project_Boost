a0aa3711fe226a69808106d9c4466d26000e45cc
Matthies <ties1000@live.nl>
2020-04-11 15:00:12 +0200

    Fixing rocket collissions

---
+++ Assets/Scenes/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start(){
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        ProcessInput();
+    }
+
+    void ProcessInput() {
+        bool leftRotation = Input.GetKey(KeyCode.A);
+        bool rightRotation = Input.GetKey(KeyCode.D);
+
+        if (Input.GetKey(KeyCode.Space)) {  //rocket can thrust
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if(leftRotation && rightRotation) { //rocket cant rotate both ways
+            //do nothing
+        }
+        else if(leftRotation) {
+            print("Rotating left");
+        }
+        else if (rightRotation) {
+            print("Rotating Right");
+        }
+
+    }
+}
